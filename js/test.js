3e2b534645c5b50e9da3f07f47a63a58d5233042
‘Krishna <kmangipudi@shawtech.org>
2020-10-06 21:05:43 -0400

    initial version

---
+++ js/test.js
@@ -0,0 +1,28 @@
+﻿window.onload = init;
+
+function init()
+{
+	document.getElementById("btn1").onclick = changeH2;	
+}
+
+function changeH2()
+{
+	var elms = document.getElementsByTagName("h2");
+	
+	for (var i=0; i < elms.length; i++)
+	{
+		if ( i == 0)
+			elms[i].innerHTML = "Changed 1";
+		else
+			elms[i].innerHTML = "Changed 2";
+
+	}
+}
+
+function displayInput()
+{
+	var text;
+	text = document.getElementById("email").value;
+	
+	document.getElementById("display").innerHTML = text;
+}
